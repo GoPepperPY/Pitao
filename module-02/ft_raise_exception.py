913ae42a667cb4493672248e71df4936f1ee369d
Goncalo Pimenta <gopiment@c2r2s11.42porto.com>
2026-05-14 05:46:42 +0100

    dsadasd

--- module-02/ft_raise_exception.py
+++ module-02/ft_raise_exception.py
@@ -5,7 +5,12 @@ def	input_temperature(temp_str: str) -> int:
 
 def	test_temperature(tmp: str) -> None:
 	try:
-		print(f"Temperature is now {input_temperature(tmp)}°C\n")
+		if input_temperature(tmp) >= 0 and input_temperature(tmp) <= 40:
+			print(f"Temperature is now {input_temperature(tmp)}°C\n")
+		if input_temperature(tmp) > 40:
+			print(f"Caught input_temperature error: {tmp} is too hot for plants (max 40°C)\n")
+		if input_temperature(tmp) < 0:
+			print(f"Caught input_temperature error: {tmp} is too cold for plants (min 0°C)\n")
 	except ValueError as e:
 		print(f"Caught input_temperature error: invalid literal for int() with base 10: {e}\n")
 
@@ -15,6 +20,10 @@ def	main() -> None:
 	test_temperature("25")
 	print("Input data is 'abc'")
 	test_temperature("abc")
+	print("Input data is '100'")
+	test_temperature("100")
+	print("Input data is '-50'")
+	test_temperature("-50")
 	print("All tests completed - program didn't crash!")
 
 if __name__ == "__main__":
