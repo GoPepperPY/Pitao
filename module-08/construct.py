ad4eb318e7dd212b0e780448a64319a2eb7814c9
Goncalo Pimenta <gopiment@c2r6s5.42porto.com>
2026-07-08 05:53:35 +0100

    Dsad

--- module-08/construct.py
+++ module-08/construct.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python3
 
+from os import environ, path
+import sys
+import site
+
+def main() -> None:
+    if not environ.get('VIRTUAL_ENV'):
+        print("MATRIX STATUS: You're still plugged in")
+        print(" -> Current Python: " + sys.executable)
+        print("\n Virtual Environment: None Detected")
+        print("The machines can see everything you install.")
+
+        print(  "\nTo enter the construct, run:"
+                "\npython -m venv matrix_env"
+                "\nsource matrix_env/bin/activate # On Unix"
+                "\nmatrix_env\Scripts\activate # On Windows"
+                "\nThen run this program again.")
+    else:
+        print("MATRIX STATUS: Welcome to the construct")
+        print(" -> Current Python: " + sys.executable)
+        print(" -> Virtual Environment: " + path.basename(environ.get('VIRTUAL_ENV')))
+        print(  "\nSUCCESS: You're in an isolated environment!\n"
+                "Safe to install packages without affecting\n"
+                "the global system.")
+        print("Package installation path:\n" +
+              site.getsitepackages()[0])
+
+if __name__ == "__main__":
+    main()
---
+++ module-08/ex1/loading.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import importlib.util
+
+REQUIRED = ["pandas", "numpy", "matplotlib"]
+
+def main() -> None:
+    print("\nLOADING STATUS: Loading programs..")
+    print("\nChecking dependencies:")
+
+if __name__ == "__main__":
+    main()
